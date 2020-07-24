57e8260a16391deda79a43795709dda7f455ba56
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-07-24 09:50:29 -0400

    fix priority

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -10,18 +10,21 @@ mongo --host mongo1:27017 <<EOF
   "members": [
       {
         "_id": 0,
-        "host": "mongo1:27017"
+        "host": "mongo1:27017",
+	      "priority": 2,
       },
       {
         "_id": 1,
-        "host": "mongo2:27018"
+        "host": "mongo2:27018",
+	      "priority": 0,
       },
       {
         "_id": 2,
-        "host": "mongo3:27019"
+        "host": "mongo3:27019",
+	      "priority": 0,
       }
     ]
   };
-  
+
   rs.initiate(cfg);
 EOF
