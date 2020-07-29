bb90353237f96d44ee1af49a6f5c451ff319650c
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-07-29 15:22:14 -0400

    ajustes

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -4,23 +4,23 @@ sleep 10
 
 echo mongo_setup.sh time now: `date +"%T" `
 
-mongo --host mongo1:27017 <<EOF
+mongo --host mongo1:28017 <<EOF
   var cfg = {
   "_id": "rs0",
   "members": [
       {
         "_id": 0,
-        "host": "mongo1:27017",
+        "host": "mongo1:28017",
 	      "priority": 2,
       },
       {
         "_id": 1,
-        "host": "mongo2:27018",
+        "host": "mongo2:28018",
 	      "priority": 0,
       },
       {
         "_id": 2,
-        "host": "mongo3:27019",
+        "host": "mongo3:28019",
 	      "priority": 0,
       }
     ]
