9013586b5cd0b2b256358fe0eccb5b4c79636847
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-07-30 10:06:55 -0400

    add setup image

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git rev-parse --abbrev-ref HEAD
+git fetch --all --tags --prune --force
+
+LATEST_VERSION=`git tag --sort=taggerdate | tail -1`
+read -p "Qual versão do deploy ($LATEST_VERSION): " VERSION
+VERSION=${VERSION:-$LATEST_VERSION}
+
+git checkout tags/$VERSION
+
+docker build -t registry.tce.ro.gov.br/mongo-cluster-setup:$VERSION .
+docker push registry.tce.ro.gov.br/mongo-cluster-setup:$VERSION
