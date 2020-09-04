92c7560387b4ad042bbd16b67d68b51cdbc44b28
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-09-04 15:49:08 -0400

    ajustes no cluster

--- mongo_setup.sh
+++ mongo_setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-echo "sleeping for 10 seconds"
-sleep 10
+
+echo "**********************************************"
+echo "Waiting for startup.."
+
+until curl http://mongo1:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+  printf '.'
+  sleep 2
+done
 
 echo mongo_setup.sh time now: `date +"%T" `
 
 mongo --host mongo1:27017 <<EOF
   var cfg = {
   "_id": "rs0",
+  "writeConcernMajorityJournalDefault" : true,
   "members": [
       {
         "_id": 0,
@@ -15,16 +22,18 @@ mongo --host mongo1:27017 <<EOF
       },
       {
         "_id": 1,
-        "host": "mongo2:27018",
-	      "priority": 0,
-      },
-      {
-        "_id": 2,
-        "host": "mongo3:27019",
+        "host": "mongo2:27017",
 	      "priority": 0,
       }
-    ]
+    ],
+    settings: {
+      chainingAllowed: true
+    }
   };
 
-  rs.initiate(cfg);
+  rs.initiate(cfg, { force: true });
+  rs.reconfig(cfg, { force: true });
+  rs.slaveOk();
+  db.getMongo().setReadPref('nearest');
+  db.getMongo().setSlaveOk();
 EOF
